b9eea359db7faadfbb526b8f59ccb1b42dcb310e
Adam Brown <adam@deftnerd.com>
2017-04-21 10:54:25 -0400

    improved onion validation to a single regex

--- src/CryptoAddressServiceProvider.php
+++ src/CryptoAddressServiceProvider.php
@@ -50,13 +50,7 @@ class CryptoAddressServiceProvider extends ServiceProvider
 
         // onion address validator
         Validator::extend('onion', function ($attribute, $value, $parameters, $validator) {
-
-            $conditions = [];
-            $conditions[] = strlen($value) == 22;
-            $conditions[] = substr(strtoupper($value), -6) == ".ONION";
-            $conditions[] = preg_match('#^(?:[A-Z2-7]{8})*(?:[A-Z2-7]{2}={6}|[A-Z2-7]{4}={4}|[A-Z2-7]{5}={3}|[A-Z2-7]{7}=)?$#', substr(strtoupper($value), 0, 16));
-
-            return (bool) array_product($conditions);
+            return (bool) preg_match('^(?i)(https?:\/\/)?(www\.)?([A-Z2-7]{16})\.?(onion)^', $value);
         });
 
     }
